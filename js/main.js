cb52ac470b3413e6984deb7c2437a19960bf02a9
FabrizioFavia <personal.favia@gmail.com>
2023-05-08 15:39:12 +0200

    feat(js): inserted image's input

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@
       createApp({
         data() {
           return {
-            message: 'Hello',
-            name: 'Fabrizio',
-            age: null,
-            mail: 'personal.favia.@gmail.com',
-            textColor: "red"
+            message: 'Ciao',
+            name: '',
+            surname: '',
+            age: '',
+            textColor: 'red',
+            display: "block",
+            image: '', /* L'Immagine verrà caricata dall'utente dopo aver inserito l'url nell'imput "immagine" --> IMMAGINE SAMPLE https://images.pexels.com/photos/3944091/pexels-photo-3944091.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 */
+            dimension: "imageContainer"
           }
         }
       }).mount('#app')
